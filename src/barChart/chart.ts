fca993340b582b64ce565496f7bf686547378f9b
Mateusz Jonak <mateusz.jonak@gmail.com>
2021-01-26 16:27:12 +0100

    step one

--- src/barChart/chart.ts
+++ src/barChart/chart.ts
@@ -1,6 +1,4 @@
 import * as d3 from 'd3';
-import { BaseType } from 'd3';
-import { translation } from '../data';
 import { ChartProps, Item } from './types';
 
 const margin = { top: 10, right: 10, bottom: 30, left: 40 };
@@ -16,79 +14,11 @@ export const createChart = ({ width, height, data, keys }: ChartProps) => {
     .padding(0.1)
     .round(true);
 
+  const yDomainMax = d3.max(series, (d1) => d3.max(d1, (d2) => d2[1]));
   const y = d3
     .scaleLinear()
-    .domain([0, d3.max(series, (d1) => d3.max(d1, (d2) => d2[1]))])
+    .domain([0, yDomainMax])
     .rangeRound([height - margin.bottom, margin.top]);
-
-  const color = d3
-    .scaleOrdinal<string>()
-    .domain(series.map((d) => d.key))
-    .range(d3.schemeSpectral[series.length])
-    .unknown('#ccc');
-
-  const appendTitle = (
-    rect: d3.Selection<
-      d3.BaseType | SVGRectElement,
-      d3.SeriesPoint<Item>,
-      d3.BaseType | SVGGElement,
-      d3.Series<Item, string>
-    >,
-  ) =>
-    rect.append('title').text(function (d) {
-      const rect = this.parentNode;
-      const g = rect.parentNode;
-      const key = d3.select<BaseType, Item>(g).datum().key;
-
-      return `${d.data.name} - ${translation[key]}`;
-    });
-
-  const transitionStackedBars = (
-    rect: d3.Selection<
-      d3.BaseType | SVGRectElement,
-      d3.SeriesPoint<Item>,
-      d3.BaseType | SVGGElement,
-      d3.Series<Item, string>
-    >,
-  ) => {
-    rect
-      .transition()
-      .duration(400)
-      .delay((d, i) => i * 10)
-      .attr('y', (d) => y(d[1]))
-      .attr('height', (d) => y(d[0]) - y(d[1]));
-  };
-
-  const appendBars = (
-    g: d3.Selection<
-      d3.BaseType | SVGGElement,
-      d3.Series<Item, string>,
-      SVGGElement,
-      undefined
-    >,
-  ) =>
-    g
-      .selectAll('rect')
-      .data((d) => d)
-      .join('rect')
-      .attr('x', (d) => x(d.data.name))
-      .attr('y', () => height - margin.bottom)
-      .attr('height', 0)
-      .attr('width', x.bandwidth())
-      .call(transitionStackedBars)
-      .call(appendTitle);
-
-  const appendSeries = (
-    g: d3.Selection<SVGGElement, undefined, null, undefined>,
-  ) => {
-    g.selectAll('g')
-      .data(series)
-      .join('g')
-      .attr('fill', (d) => color(d.key))
-      .call(appendBars);
-  };
-
-  svg.append('g').call(appendSeries);
 
   svg
     .append('g')
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import { createChart, createChartLegend } from './barChart';
+import { createChart } from './barChart';
 import { data, keys } from './data';
 
 const chart = createChart({ width: 400, height: 500, data, keys });
-const chartLegend = createChartLegend({
-  width: 400,
-  height: 240,
-  data,
-  keys,
-});
 
 document.querySelector('body').append(chart.node());
-document.querySelector('body').append(chartLegend.node());
